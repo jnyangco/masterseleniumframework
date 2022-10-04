b48ac27a7dec1b967e9181cfa03f45b2dc6734bb
jeromeyangco1 <yangco.jerome@gmail.com>
2022-10-05 05:02:12 +0800

    18_AddedTestNGXML

--- src/test/java/org/selenium/pom/base/BaseTest.java
+++ src/test/java/org/selenium/pom/base/BaseTest.java
@@ -4,13 +4,15 @@ import org.openqa.selenium.WebDriver;
 import org.selenium.pom.factory.DriverManager;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Parameters;
 
 public class BaseTest {
     protected WebDriver driver;
 
+    @Parameters("browser")
     @BeforeTest
-    public void startDriver() {
-        driver = new DriverManager().initializeDriver();
+    public void startDriver(String browser) {
+        driver = new DriverManager().initializeDriver(browser);
     }
 
     @AfterTest
--- src/test/java/org/selenium/pom/factory/DriverManager.java
+++ src/test/java/org/selenium/pom/factory/DriverManager.java
@@ -10,12 +10,20 @@ import java.time.Duration;
 
 public class DriverManager {
 
-    public WebDriver initializeDriver() {
+    public WebDriver initializeDriver(String browser) {
         //System.setProperty("webdriver.chrome.driver", "C:\\Selenium Configuration\\browser_jars\\chromedriver.exe");
         //This line of code will set up the chromedriver automatically
 //        WebDriverManager.chromedriver().setup();
         WebDriver driver;
-        String browser = System.getProperty("browser", "CHROME");//if null - set default to CHROME
+        //String browser = System.getProperty("browser", "CHROME");//if null - set default to CHROME
+
+//        String localBrowser;
+//        localBrowser = System.getProperty("browser");//maven
+//        localBrowser = browser;//testng.xml
+
+        //1-maven, 2-testng (maven=null) then get from testng.xml, 3-from config file (so you can run by right-clicking on method name)
+        //Usage: 1-maven only -> CICD, 2-testng.xml only, 3-maven with testng.xml
+        browser = System.getProperty("browser", browser);
 
         //switch (browser.toLowerCase()) { }
         switch (BrowserType.valueOf(browser)) { //converting browser string to enum constants
